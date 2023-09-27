ad3220ccb72a7a52dafec335b884e434e91f70ea
Adolfo Chafardett <adolfo1997a@gmail.com>
2023-09-27 11:34:00 -0400

    receiving frontend variables

--- src/scripts/agregarTema.js
+++ src/scripts/agregarTema.js
@@ -3,7 +3,7 @@ var form = document.getElementById('form');
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     var tema = document.getElementById('tema').value;
-    fetch('http://localhost:3000/src/api/busqueda_principal.php', {
+    fetch('http://localhost:8080/src/api/busqueda_principal.php', {
         method: 'POST',
         body: JSON.stringify({
             tema: tema
