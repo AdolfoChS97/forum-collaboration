dd03f7fe236d7cc895564354361f72d0986e993b
Adolfo Chafardett <adolfo1997a@gmail.com>
2023-10-03 15:57:29 -0400

    feat: updated main script

--- src/scripts/agregarTema.js
+++ src/scripts/agregarTema.js
@@ -1,17 +1,10 @@
-var form = document.getElementById('form');
+import { fetchJson } from '../client/utils/fetchJson.js'
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    var tema = document.getElementById('tema').value;
-    fetch('http://localhost:8080/src/api/busqueda_principal.php', {
-        method: 'POST',
-        body: JSON.stringify({
-            tema: tema
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(function (response) {
-        console.log(response);
-    });
+var form = document.getElementById('form')
+
+form.addEventListener('submit', async function (e) {
+    e.preventDefault()
+    const tema = document.getElementById('tema').value
+    const { data, statusCode } = await fetchJson('http://localhost:8080/src/api/MainSearch.php', 'POST', {tema: tema})
+    
 });
