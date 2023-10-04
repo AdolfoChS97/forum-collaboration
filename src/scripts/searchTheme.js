e8a8d0782237a98593c163eff10102beafc57fa1
Adolfo Chafardett <adolfo1997a@gmail.com>
2023-10-04 10:40:22 -0400

    feat: we're printing a table in our index accordint to the user search

--- src/scripts/searchTheme.js
+++ src/scripts/searchTheme.js
@@ -1,4 +1,5 @@
 import { fetchJson } from '../client/utils/fetchJson.js'
+import { createTable } from '../client/utils/createTable.js'
 
 var form = document.getElementById('form')
 
@@ -6,5 +7,6 @@ form.addEventListener('submit', async function (e) {
     e.preventDefault()
     const params = new URLSearchParams(`theme=${document.getElementById('tema').value}`).toString()
     const { data, statusCode } = await fetchJson(`http://${window.location.host}/src/api/Search.php?${params}`, 'GET',)
-    console.log(data, statusCode);
+    createTable(data, ['ID', 'titulo', 'tema', 'autor', 'created_at', 'updated_at'])
+
 });
